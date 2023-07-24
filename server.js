e02c5ac33426cb5f79b04584dfb3e7eae00b0583
Benbradhurst <s223438554@deakin.edu.au>
2023-07-24 11:57:18 +1000

    Functioning 1st draft

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express')
 var app = express()
+
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 var port = process.env.port || 3000;
+
+app.get('/',(req, res)=>{
+    res.sendFile('index.html')
+})
 
 app.listen(port,()=>{
     console.log("App listening to: "+ port)
-    console.log("Server live at localhost."+port)
+    console.log("Server live at https//:localhost."+port)
+    console.log("Type Ctrl+C to shut down");
 })
+
