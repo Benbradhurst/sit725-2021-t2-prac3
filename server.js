a2ff2e00e553f13735625c39ffb833db9d8ee4e3
Benbradhurst <s223438554@deakin.edu.au>
2023-07-27 18:43:37 +1000

    functioning copy

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ var app = express()
 
 app.use(express.static(__dirname+'/public'))
 app.use(express.json());
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:false}));
 
 var port = process.env.port || 3000;
 
 app.get('/',(req, res)=>{
-    res.sendFile('index.html')
+    res.sendFile('views/pages/index')
 })
 
 app.listen(port,()=>{
     console.log("App listening to: "+ port)
-    console.log("Server live at https//:localhost."+port)
+    console.log("Server live at http://localhost:"+port)
     console.log("Type Ctrl+C to shut down");
 })
 
